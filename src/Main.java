a9a0d4ba8cbddc0c498eee12f86b10fb647d3b79
RejVasi <149894969+RejVasi@users.noreply.github.com>
2026-03-26 20:41:57 +0100

    Added comments to variables used when creating seller object

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 import java.time.LocalDate;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main
 {
     public static void main(String[] args) {
-        // Birthdate in YYYY, MM, DD
+
+        // Name, birthdate (in YYYY, MM, DD), total contracts made, sold carrots (in tons), is senior seller (boolean)
         CarrotSeller firsCarrotSeller = new CarrotSeller("Jan Novotný", LocalDate.of(1978, 1, 24), 7, 8.5, true);
         CarrotSeller secondCarrotSeller = new CarrotSeller("Marek Nový", LocalDate.of(1985, 1, 12), 2, 4.2, false);
 
