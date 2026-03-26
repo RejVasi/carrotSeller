afe47fedbc9bc10151ece2d82cf9bac3b8572623
RejVasi <149894969+RejVasi@users.noreply.github.com>
2026-03-26 20:37:18 +0100

    Initial commit

--- src/CarrotSeller.java
+++ src/CarrotSeller.java
@@ -1,4 +1,76 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
-public class CarrotSeller {
+public class CarrotSeller
+{
+    private String sellerName;
+    private LocalDate birthDate;
+    private int contractsMade;
+    private double soldCarrots; // in tons
+    private boolean isSeniorSeller;
+
+    public CarrotSeller(String _name, LocalDate _date, int _contractsMade, double _soldCarrots, boolean _isSeniorSeller)
+    {
+        this.sellerName = _name;
+        this.birthDate = _date;
+
+        this.contractsMade = _contractsMade;
+        this.soldCarrots = _soldCarrots;
+        this.isSeniorSeller = _isSeniorSeller;
+    }
+
+
+    //  Getters
+    public String getSellerName()
+    {
+        return this.sellerName;
+    }
+
+    public LocalDate getBirthDate()
+    {
+        return this.birthDate;
+    }
+
+    public int getContractsMade()
+    {
+        return this.contractsMade;
+    }
+
+    public double getSoldCarrots()
+    {
+        return this.soldCarrots;
+    }
+
+    public boolean isSeniorSeller()
+    {
+        return this.isSeniorSeller;
+    }
+
+
+    //  Setters
+    public void setSellerName(String sellerName)
+    {
+        this.sellerName = sellerName;
+    }
+
+    public void setBirthDate(LocalDate birthDate)
+    {
+        this.birthDate = birthDate;
+    }
+
+    public void setContractsMade(int contractsMade)
+    {
+        this.contractsMade = contractsMade;
+    }
+
+    public void setSoldCarrots(double soldCarrots)
+    {
+        this.soldCarrots = soldCarrots;
+    }
+
+    public void setSeniorSeller(boolean seniorSeller)
+    {
+        isSeniorSeller = seniorSeller;
+    }
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Birthdate in YYYY, MM, DD
+        CarrotSeller firsCarrotSeller = new CarrotSeller("Jan Novotný", LocalDate.of(1978, 1, 24), 7, 8.5, true);
+        CarrotSeller secondCarrotSeller = new CarrotSeller("Marek Nový", LocalDate.of(1985, 1, 12), 2, 4.2, false);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(firsCarrotSeller.getSellerName());
+        System.out.println(secondCarrotSeller.getSellerName());
+
     }
 }
